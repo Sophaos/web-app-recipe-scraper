5609c4d37a5403e5a673bae1f1fbf3ca57b56689
Son-Thang Pham <54873061+Sophaos@users.noreply.github.com>
2025-02-27 01:11:22 -0500

    switched to vue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [vue(), viteTsconfigPaths()],
   server: {
     host: true,
   },
